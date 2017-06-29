96f0c25d0780606a151e5662103112d7c078944b
Joseph Mtinangi <josephmtinangi@gmail.com>
2017-06-29 16:38:34 +0300

    Create Newsletter Mail

---
+++ app/Mail/Newsletter.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Mail;
+
+use App\Models\Newsletter as NewsletterModel;
+use Illuminate\Bus\Queueable;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Contracts\Queue\ShouldQueue;
+
+class Newsletter extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    public $newsletter;
+
+    /**
+     * Create a new message instance.
+     *
+     * @param Newsletter $newsletter
+     */
+    public function __construct(NewsletterModel $newsletter)
+    {
+        $this->newsletter = $newsletter;
+    }
+
+    /**
+     * Build the message.
+     *
+     * @return $this
+     */
+    public function build()
+    {
+        return $this->view('emails.newsletters.show');
+    }
+}
