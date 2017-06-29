76fc59ecee49a765d9af26d0a0c68ef9aa179bf8
Joseph Mtinangi <josephmtinangi@gmail.com>
2017-06-29 15:50:15 +0300

    Create NewsletterSubscriber model factory

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -22,3 +22,11 @@ $factory->define(App\User::class, function (Faker\Generator $faker) {
         'remember_token' => str_random(10),
     ];
 });
+
+$factory->define(App\Models\NewsletterSubscriber::class, function (Faker\Generator $faker) {
+
+    return [
+        'email' => $faker->freeEmail,
+        'verified' => true,
+    ];
+});
