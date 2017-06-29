66bd2bf37cfcaf0f42e3dfb646aed4bfd1f6a02a
Joseph Mtinangi <josephmtinangi@gmail.com>
2017-06-29 19:57:09 +0300

    Create jobs table

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,40 +2,6 @@
 
 @section('content')
 
-    <div class="jumbotron">
-        <div class="container">
-            <h1>Mail Automation</h1>
-            <p>
-            	Lorem ipsum dolor sit amet, consectetur adipisicing elit. Temporibus, soluta!
-            </p>
-    	</div>
-    </div>
 
-    <div class="container-fluid">
-    	<div class="features">
-	    	<div class="row">
-	    		<div class="col-sm-4">
-	    			<div class="feature"></div>
-	    		</div>
-	    		<div class="col-sm-4">
-	    			<div class="feature"></div>
-	    		</div>
-	    		<div class="col-sm-4">
-	    			<div class="feature"></div>
-	    		</div>
-	    	</div>
-	    	<div class="row">
-	    		<div class="col-sm-4">
-	    			<div class="feature"></div>
-	    		</div>
-	    		<div class="col-sm-4">
-	    			<div class="feature"></div>
-	    		</div>
-	    		<div class="col-sm-4">
-	    			<div class="feature"></div>
-	    		</div>
-	    	</div>	    	
-    	</div>
-    </div>
 
 @endsection
